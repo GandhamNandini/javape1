2fd646f81020c192d70d3e3c44e81246d9a4f137
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-03 16:21:32 +0530

    Update Ascii.java

--- src/main/java/com/stackroute/exercise1/Ascii.java
+++ src/main/java/com/stackroute/exercise1/Ascii.java
@@ -2,15 +2,15 @@ package com.stackroute.exercise1;
 
 public class Ascii {
     public String findingCharacters(char character){
-
+        //range of capital letter
         if(character>=65 && character<=90){
             return "Capital letter";
         }
-
+        //range of small letters
         else if(character>=97 && character<=122){
             return "small letter";
         }
-
+        //range of digits
         else if(character>=48 && character<=57){
             return "Digit";
         }
