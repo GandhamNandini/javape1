5be235086c45748c41911989451f441b5db78340
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 12:04:28 +0530

    Update ReverseString.java

--- src/main/java/com/stackroute/exercise1/ReverseString.java
+++ src/main/java/com/stackroute/exercise1/ReverseString.java
@@ -4,6 +4,7 @@ public class ReverseString
 {
     public String stringReverse(String original){
         String reverse="";
+        //to print the reverse of a string
         int length=original.length();
         for(int i=length-1;i>=0;i--){
             char ch=original.charAt(i);
