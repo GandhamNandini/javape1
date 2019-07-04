ebeaf45ebe9e769052e383a6ec579f7222c03aa8
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 12:01:44 +0530

    Update Finding.java

--- src/main/java/com/stackroute/exercise1/Finding.java
+++ src/main/java/com/stackroute/exercise1/Finding.java
@@ -8,8 +8,10 @@ public class Finding {
         for(int i=0;i<s.length();i++)
         {
             char  c=s.charAt(i);
+            //To check it the given string has alphabets or not
             if( (c >= 'a' && c<= 'z') || (c >= 'A' && c<= 'Z'))
             {
+                //if alphabets check if they are vowels else consonants
                 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||
                         c=='I'||c=='O'||c=='U')
                 {
