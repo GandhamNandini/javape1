2d71d0cd0bab104b719af71df4918ac700610101
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 12:05:59 +0530

    Update UnspecificInput.java

--- src/main/java/com/stackroute/exercise1/UnspecificInput.java
+++ src/main/java/com/stackroute/exercise1/UnspecificInput.java
@@ -1,8 +1,11 @@
 package com.stackroute.exercise1;
 
+import java.util.Scanner;
+
 public class UnspecificInput
 {
     public static void main(String[] args){
+        //a unspecified input is given and add the inputs
         Scanner scan=new Scanner(System.in);
         int sum=0;
         while (scan.hasNext() && (!("\n").equals(scan.hasNext()))) {
