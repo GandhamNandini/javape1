4c893fea96b1dca29f6c75c6eedd8081b32c207b
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 12:03:40 +0530

    Update Pattern.java

--- src/main/java/com/stackroute/exercise1/Pattern.java
+++ src/main/java/com/stackroute/exercise1/Pattern.java
@@ -5,6 +5,7 @@ public class Pattern
     public String  printPattern(int number){
         int j;
         int k=0;
+        //condition for printing the pattern 122333 based on the given input
         String s1="";
         for (int i = 1; i <= number; i++) {
             for (j = 1; j <= i;j++ ) {
