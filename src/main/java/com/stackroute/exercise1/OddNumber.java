2d6757ca5ba625e42b3277243aa141d6fa41088e
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 12:02:42 +0530

    Update OddNumber.java

--- src/main/java/com/stackroute/exercise1/OddNumber.java
+++ src/main/java/com/stackroute/exercise1/OddNumber.java
@@ -4,11 +4,14 @@ public class OddNumber {
     public String checkOdd(int number) {
         String s1="Tom";
         String s2="Jerry";
+        //if number is odd then print tom
         if (number % 2 != 0) {
             if (number > 20 && number < 30) {
                 return s1;
             }
         }
+        //else then print jerry
+
         else {
             if (number > 20 && number < 30) {
                 return s2;
