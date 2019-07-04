b5345a21654cb00e3189f21d44d56b7c852f899b
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 12:02:16 +0530

    Update Guess.java

--- src/main/java/com/stackroute/exercise1/Guess.java
+++ src/main/java/com/stackroute/exercise1/Guess.java
@@ -2,9 +2,11 @@ package com.stackroute.exercise1;
 
 public class Guess {
     public  String guess(int instance_num,int guess_num) {
-
+        //guessing the given number
+        //if the given number is equal to guessed number
         if (guess_num == instance_num) {
             return "Number guessed matches the original";
+            //if given number is greater than guessed number
         } else if (guess_num > instance_num) {
             return "Number guessed is greater than the original";
 
